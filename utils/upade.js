1cd7bb37332173e385b1dd54242f0c6705ede342
Tristan LaRoche <laroche11235@gmail.com>
2021-03-11 11:17:45 -0500

    made base function and exported them

--- utils/upade.js
+++ utils/upade.js
@@ -1,1 +1,14 @@
 const inquirer = require("inquirer");
+
+function updateRole(){
+
+}
+
+function updateManager(){
+
+}
+
+module.exports = {
+    updateRole: updateRole,
+    updateManager: updateManager
+}
