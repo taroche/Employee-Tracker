18ffa451f426c2ce2bb1447ef3770c678fa8a987
Tristan LaRoche <laroche11235@gmail.com>
2021-03-11 11:01:27 -0500

    set up connection to mysql server

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+})
+
