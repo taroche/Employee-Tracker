11ad0c160c06aab79d528ddd0748a66d8e468b23
Tristan LaRoche <laroche11235@gmail.com>
2021-03-11 11:14:07 -0500

    make base functions and export them

--- utils/add.js
+++ utils/add.js
@@ -1,1 +1,19 @@
 const inquirer = require("inquirer");
+
+function addEmployee(){
+
+}
+
+function addRole(){
+
+}
+
+function addDepartment(){
+
+}
+
+module.exports = {
+    addEmployee: addEmployee,
+    addRole: addRole,
+    addDepartment: addDepartment
+};
