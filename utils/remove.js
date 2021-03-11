a5980ff7f72e68d14ae45ac2342745bc40d0157b
Tristan LaRoche <laroche11235@gmail.com>
2021-03-11 11:16:04 -0500

    made base function and exports them

--- utils/remove.js
+++ utils/remove.js
@@ -1,1 +1,19 @@
 const inquirer = require("inquirer");
+
+function removeEmployee() {
+
+}
+
+function removeRole() {
+
+}
+
+function removeDepartment(){
+
+}
+
+module.exports = {
+    removeEmployee: removeEmployee,
+    removeRole: removeRole,
+    removeDepartment: removeDepartment
+}
