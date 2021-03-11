fb429a3e8388a7ec56d98c5382cc2e379915cb53
Tristan LaRoche <laroche11235@gmail.com>
2021-03-11 11:19:57 -0500

    made base function and exported them

--- utils/view.js
+++ utils/view.js
@@ -1,1 +1,29 @@
 const inquirer = require("inquirer");
+
+function viewAllEmployees(){
+
+}
+
+function viewEmployeeDept(){
+
+}
+
+function viewEmployeeMgr(){
+
+}
+
+function viewRoles(){
+
+}
+
+function viewDepartments(){
+
+}
+
+module.exports = {
+    viewAllEmployees: viewAllEmployees,
+    viewEmployeeDept: viewEmployeeDept,
+    viewEmployeeMgr: viewEmployeeMgr,
+    viewRoles: viewRoles,
+    viewDepartments: viewDepartments
+};
