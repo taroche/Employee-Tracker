64bbeb9f8cd12e1bb950b6636bebff5113b9fd54
Tristan LaRoche <laroche11235@gmail.com>
2021-03-14 17:18:45 -0400

    vuewEmployeeDept function

--- utils/view.js
+++ utils/view.js
@@ -17,8 +17,22 @@ const view = {
         })
     },
 
-    viewEmployeeDept() {
+    viewEmployeeDept(connection, init) {
+        let queryString = `
+        SELECT employee.id, first_name, last_name, title, salary, dept_name
+        FROM employee
+        LEFT JOIN roles
+        ON role_id = roles.id
+        LEFT JOIN department
+        ON department_id = department.id;`
 
+        connection.query(queryString, (err, data) => {
+            if (err) throw err;
+            console.log("\n")
+            console.table(data)
+            console.log("\n")
+            init()
+        })
     },
 
     viewEmployeeMgr() {
