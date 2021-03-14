bda251ad83e29ea7d14ff1631b4ab41ed8c8dad1
Tristan LaRoche <laroche11235@gmail.com>
2021-03-14 16:46:20 -0400

    create table for roles in db

--- utils/db/db.sql
+++ utils/db/db.sql
@@ -8,3 +8,11 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
