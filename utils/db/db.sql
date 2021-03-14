56e89447adc98e24cf7015d7617aad4a8191fb5b
Tristan LaRoche <laroche11235@gmail.com>
2021-03-14 16:52:45 -0400

    create table employee in db

--- utils/db/db.sql
+++ utils/db/db.sql
@@ -16,3 +16,14 @@ CREATE TABLE roles (
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
