6cb18023d703b83c6872c9eefb1114a25e40000c
Tristan LaRoche <laroche11235@gmail.com>
2021-03-14 17:16:33 -0400

    turn seperate function into one object

--- utils/view.js
+++ utils/view.js
@@ -1,29 +1,25 @@
 const inquirer = require("inquirer");
 
-function viewAllEmployees(){
+const view = {
 
+    viewAllEmployees(){
+    },
+    
+    viewEmployeeDept(){
+
+    },
+    
+    viewEmployeeMgr(){
+        
+    },
+    
+    viewRoles(){
+
+    },
+    
+    viewDepartments(){
+        
+    }
 }
 
-function viewEmployeeDept(){
-
-}
-
-function viewEmployeeMgr(){
-
-}
-
-function viewRoles(){
-
-}
-
-function viewDepartments(){
-
-}
-
-module.exports = {
-    viewAllEmployees: viewAllEmployees,
-    viewEmployeeDept: viewEmployeeDept,
-    viewEmployeeMgr: viewEmployeeMgr,
-    viewRoles: viewRoles,
-    viewDepartments: viewDepartments
-};
+module.exports = view
