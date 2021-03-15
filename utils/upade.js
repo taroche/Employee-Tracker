aad9056ba38cef74fdfe62b48da4f1983f3dc58e
Tristan LaRoche <laroche11235@gmail.com>
2021-03-15 16:35:52 -0400

    upadateManager function

--- utils/upade.js
+++ utils/upade.js
@@ -64,8 +64,67 @@ const update = {
         })
     },
 
-    updateManager() {
+    updateManager(connection, init) {
 
+        let newManager = {};
+
+        connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, e2.first_name AS manager FROM employee LEFT JOIN employee AS e2 ON e2.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id", function (err, results) {
+            if (err) throw err;
+            inquirer
+                .prompt([
+                    {
+                        name: "updateEmployee",
+                        type: "list",
+                        choices: function () {
+                            let choiceArray = [];
+                            for (var i = 0; i < results.length; i++) {
+                                choiceArray.push(results[i].first_name);
+                            }
+                            return choiceArray;
+                        },
+                        message: "Which employee would you like to update?"
+                    }
+                ])
+                .then(function (answer) {
+
+                    newManager.first_name = answer.updateEmployee;
+
+                    connection.query("SELECT * FROM employee", function (err, res) {
+                        if (err) throw err;
+                        inquirer
+                            .prompt([
+                                {
+                                    name: "updateManager",
+                                    type: "list",
+                                    choices: function () {
+                                        let choiceArray = [];
+                                        for (var i = 0; i < results.length; i++) {
+                                            choiceArray.push(results[i].first_name);
+                                        }
+                                        return choiceArray;
+                                    },
+                                    message: "Who would you like to change their manager to?"
+                                }
+                            ])
+                            .then(function (answer) {
+                                connection.query("SELECT * FROM employee WHERE first_name = ?", answer.updateManager, function (err, results) {
+                                    if (err) throw err;
+
+                                    newManager.manager_id = results[0].id;
+
+                                    connection.query("UPDATE employee SET manager_id = ? WHERE first_name = ?", [newManager.manager_id, newManager.first_name], function (err, res) {
+                                        if (err) throw (err);
+                                        console.log("\n");
+                                        console.log('Employee manager successfully updated.');
+                                        console.log("\n");
+                                        init();
+                                    })
+
+                                })
+                            });
+                    });
+                });
+        })
     }
 }
 
