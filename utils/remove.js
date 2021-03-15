04ac183fc7dd15d0cca35a6fcb7e0c68ee3df84c
Tristan LaRoche <laroche11235@gmail.com>
2021-03-15 16:16:07 -0400

    function removeEmployee

--- utils/remove.js
+++ utils/remove.js
@@ -1,8 +1,33 @@
 const inquirer = require("inquirer");
 
 const remove = {
-    removeEmployee() {
-
+    removeEmployee(connection, init) {
+        connection.query("SELECT * FROM employee", function (err, results) {
+            if (err) throw err;
+            inquirer
+                .prompt([
+                    {
+                        name: "removeEmployee",
+                        type: "list",
+                        choices: function () {
+                            let choiceArray = [];
+                            for (var i = 0; i < results.length; i++) {
+                                choiceArray.push(results[i].first_name);
+                            }
+                            return choiceArray;
+                        },
+                        message: "Which employee would you like to remove?"
+                    }
+                ])
+                .then(function (answer) {
+                    let query = 'DELETE FROM employee WHERE first_name = ?;'
+                    connection.query(query, answer.removeEmployee, function (err, res) {
+                        if (err) throw err;
+                        console.log("Employee successfully deleted");
+                        init();
+                    });
+                });
+        });
     },
 
     removeRole() {
