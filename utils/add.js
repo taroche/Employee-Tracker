9dc660400325c0fac6e73ce8074cb4dc3542b6e7
Tristan LaRoche <laroche11235@gmail.com>
2021-03-15 16:24:00 -0400

    function to poplate arrays with department name

--- utils/add.js
+++ utils/add.js
@@ -1,9 +1,28 @@
 const inquirer = require("inquirer");
+let departments = []
+let departmentsId = []
 
 const add = {
 
     addEmployee(connection, init){
 
+    },
+    
+    populateDepartments(connection) {
+        departments = []
+        departmentsId = []
+
+        let queryString = `
+        SELECT *
+        FROM department`
+
+        connection.query(queryString, (err, data) => {
+            if (err) throw err
+            data.forEach(element => {
+                departments.push(element.dept_name)
+                departmentsId.push(element.id)
+            });
+        })
     },
 
     addRole(connection, init){
