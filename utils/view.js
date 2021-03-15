7e79b39d644afa402f65ddc5fa845571f288ce1c
Tristan LaRoche <laroche11235@gmail.com>
2021-03-15 16:38:23 -0400

    fixed viewEmployeeMgr

--- utils/view.js
+++ utils/view.js
@@ -36,19 +36,35 @@ const view = {
     },
 
     viewEmployeeMgr(connection, init) {
-        let queryString = `
-        SELECT employee.id, first_name, last_name, manager_id
-        FROM employee
-        LEFT JOIN employee
-        ON manager_id = manager_id;`
-
-        connection.query(queryString, (err, data) => {
+        connection.query("SELECT DISTINCT e2.first_name, e2.last_name FROM employee LEFT JOIN employee AS e2 ON employee.manager_id = e2.id WHERE e2.first_name IS NOT NULL", function (err, results) {
             if (err) throw err;
-            console.log("\n")
-            console.table(data)
-            console.log("\n")
-            init()
-        })
+            inquirer
+                .prompt([
+                    {
+                        name: "manager",
+                        type: "list",
+                        choices: function () {
+                            let choiceArray = [];
+                            for (var i = 0; i < results.length; i++) {
+                                choiceArray.push(results[i].first_name);
+                            }
+                            return choiceArray;
+                        },
+                        message: "Which manager would you like to search by?"
+                    }
+                ])
+                .then(function (answer) {
+                    console.log(answer.manager);
+                    let query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, e2.first_name AS manager FROM employee LEFT JOIN employee AS e2 ON e2.id = employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id WHERE e2.first_name = ? ORDER BY employee.id;'
+                    connection.query(query, answer.manager, function (err, data) {
+                        if (err) throw err;
+                        console.log("\n");
+                        console.table(data);
+                        console.log("\n");
+                        init();
+                    });
+                });
+        });
     },
 
     viewRoles(connection, init) {
