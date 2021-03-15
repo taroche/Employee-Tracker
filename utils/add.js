a38894016a9ae460bdeb46bdf112f652983ca020
Tristan LaRoche <laroche11235@gmail.com>
2021-03-15 16:29:18 -0400

    addEmployee function

--- utils/add.js
+++ utils/add.js
@@ -5,7 +5,89 @@ let departmentsId = []
 const add = {
 
     addEmployee(connection, init){
-
+        let newEmployee = {};
+        connection.query("SELECT * FROM role", function (err, results) {
+            if (err) throw err;
+            inquirer
+                .prompt([
+                    {
+                        name: "first_name",
+                        type: "input",
+                        message: "What is the employee's first name?",
+                        validate: function (answer) {
+                            if (answer.length < 1) {
+                                return console.log("A valid first name is required.");
+                            }
+                            return true;
+                        }
+                    },
+                    {
+                        name: "last_name",
+                        type: "input",
+                        message: "What is the employee's last name?",
+                        validate: function (answer) {
+                            if (answer.length < 1) {
+                                return console.log("A valid last name is required.");
+                            }
+                            return true;
+                        }
+                    },
+                    {
+                        name: "role",
+                        type: "list",
+                        choices: function () {
+                            let choiceArray = [];
+                            for (var i = 0; i < results.length; i++) {
+                                choiceArray.push(results[i].title);
+                            }
+                            return choiceArray;
+                        },
+                        message: "What is the employee's role?"
+                    }
+                ])
+                .then(function (answer) {
+    
+                    newEmployee.first_name = answer.first_name;
+                    newEmployee.last_name = answer.last_name;
+    
+                    connection.query("SELECT * FROM role WHERE title = ?", answer.role, function (err, results) {
+                        if (err) throw err;
+    
+                        newEmployee.role_id = results[0].id;
+                        connection.query("SELECT * FROM employee;", function (err, results) {
+                            if (err) throw err;
+                            inquirer
+                                .prompt([
+                                    {
+                                        name: "manager_name",
+                                        type: "list",
+                                        choices: function () {
+                                            let choiceArray = [];
+                                            for (var i = 0; i < results.length; i++) {
+                                                choiceArray.push(results[i].first_name);
+                                            }
+                                            return choiceArray;
+                                        },
+                                        message: "Who is the employee's manager?"
+                                    }
+                                ])
+                                .then(function (answer) {
+                                    connection.query("SELECT id FROM employee WHERE first_name = ?", answer.manager_name, function (err, results) {
+                                        if (err) throw err;
+                                        newEmployee.manager_id = results[0].id;
+                                        console.log("Adding new employee: ", newEmployee);
+    
+                                        connection.query('INSERT INTO employee SET ?', newEmployee, function (err, results) {
+                                            if (err) throw err;
+                                            console.log("Employee successfully added.");
+                                            init();
+                                        })
+                                    })
+                                });
+                        });
+                    });
+                });
+        })
     },
 
     populateDepartments(connection) {
