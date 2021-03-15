98263c70c5fc7096abd8954b49983780a6be9de5
Tristan LaRoche <laroche11235@gmail.com>
2021-03-15 16:22:57 -0400

    addDepartment function

--- utils/add.js
+++ utils/add.js
@@ -10,7 +10,23 @@ const add = {
         
     },
     
-    addDepartment(connection, init){
+    addDepartment(connection, init) {
+        inquirer.prompt([
+            {
+                name: "newDept",
+                message: "What is the new Department that we're adding?"
+            }
+        ]).then(response => {
+            let queryString = `
+            INSERT INTO department (dept_name)
+            VALUES (?)`
+
+            connection.query(queryString, [response.newDept], (err, data) => {
+                if (err) throw err
+                console.log(data)
+                init()
+            })
+        })
     } 
 }
 
